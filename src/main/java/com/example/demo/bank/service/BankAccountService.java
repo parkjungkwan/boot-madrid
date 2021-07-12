b6de865896f47e83021b53fe57ea9f57637eea5f
parkjungkwan <pakjkwan@gmail.com>
2021-07-12 16:53:12 +0900

    16

--- src/main/java/com/example/demo/bank/service/BankAccountService.java
+++ src/main/java/com/example/demo/bank/service/BankAccountService.java
@@ -1,4 +1,7 @@
 package com.example.demo.bank.service;
+
+import com.example.demo.bank.domain.BankAccountDTO;
+
 /**
  * * 은행이름은 상수로 정한다.
  이름, 금액, 계좌번호 을 입력한후 입금한 후 잔액을 확인한다
@@ -12,5 +15,9 @@ package com.example.demo.bank.service;
  * */
 public interface BankAccountService {
 
-
+    void createAccount(BankAccountDTO bank);
+    int findBalance(BankAccountDTO bank);
+    int deposit(BankAccountDTO bank);
+    int withdraw(BankAccountDTO bank);
+    void dropAccount(BankAccountDTO bank);
 }
--- src/main/java/com/example/demo/bank/service/BankAccountServiceImpl.java
+++ src/main/java/com/example/demo/bank/service/BankAccountServiceImpl.java
@@ -3,7 +3,34 @@ package com.example.demo.bank.service;
 import com.example.demo.bank.domain.BankAccountDTO;
 
 public class BankAccountServiceImpl implements BankAccountService{
-    BankAccountDTO bankAccount = new BankAccountDTO();
+    private BankAccountDTO bankAccount;
+
+    @Override
+    public void createAccount(BankAccountDTO bank) {
+        bankAccount = new BankAccountDTO();
+    }
+
+    @Override
+    public int findBalance(BankAccountDTO bank) {
+        return bankAccount.getMoney();
+    }
+
+    @Override
+    public int deposit(BankAccountDTO bank) {
+        int restMoney = bankAccount.getMoney();
+        bankAccount.setMoney(restMoney + bank.getMoney());
+        return bankAccount.getMoney();
+    }
+
+    @Override
+    public int withdraw(BankAccountDTO bank) {
+        return 0;
+    }
+
+    @Override
+    public void dropAccount(BankAccountDTO bank) {
+
+    }
 }
 
 /*
