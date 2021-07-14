e24bea0a2318bdc82c6faecfdba0f5bd44d3c30b
parkjungkwan <pakjkwan@gmail.com>
2021-07-14 17:35:19 +0900

    19

--- src/main/java/kr/scalar/api/bank/domain/BankAccountDTO.java
+++ src/main/java/kr/scalar/api/bank/domain/BankAccountDTO.java
@@ -13,5 +13,13 @@ public class BankAccountDTO {
     private float interest;
     public static final String BANK_NAME = "비트은행";
 
-
+    @Override
+    public String toString() {
+        return BANK_NAME + " : "+
+                "예금주='" + name + '\'' +
+                ", 계좌번호='" + accountNumber + '\'' +
+                ", 잔고=" + balance +
+                ", 거래일자='" + date + '\'' +
+                ", 이자율=" + interest +"\n";
+    }
 }
